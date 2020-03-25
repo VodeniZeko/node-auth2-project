3737a58c8312b4343fc3ff28f066e5a63ece6a74
VodeniZeko <salexy.es@gmail.com>
2020-03-24 21:52:05 -0700

    finished MVP

---
+++ user/user-router.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+
+const Users = require("./user-model.js");
+const restricted = require("../auth/restricted.js");
+const getUser = require("../auth/middleware.js");
+
+router.get("/", restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+router.get("/department", restricted, getUser, (req, res) => {
+  const department = req.user.department;
+  Users.findBy({ department })
+    .then(users => {
+      if (users.length === 0) {
+        res.status(404).json({ message: "No users in specified department." });
+      } else {
+        res.status(200).json(users);
+      }
+    })
+    .catch(err =>
+      res.status(500).json({ error: "Server error, could not get users." })
+    );
+});
+
+module.exports = router;
